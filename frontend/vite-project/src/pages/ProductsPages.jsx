703a8b565074af59be6eb0c57644034fbc94f74f
DIPAKK2310 <dipakkhare217@gmail.com>
2025-08-29 21:59:33 +0530

    Added right endpoints for apis

--- frontend/vite-project/src/pages/ProductsPages.jsx
+++ frontend/vite-project/src/pages/ProductsPages.jsx
@@ -21,7 +21,7 @@ export default function ProductsPage() {
   useEffect(() => {
     const fetchApi = async () => {
       try {
-        const response = await axios.get('http://localhost:4000/api/product/products');
+        const response = await axios.get('http://localhost:4000/api/products');
         setProducts(response.data);
         setFilteredData(response.data); // Initially, display all products
       } catch (error) {
