9238e4c0c30273a2f0c102b7f129fbb46f9dd545
DIPAKK2310 <dipakkhare217@gmail.com>
2026-04-05 22:32:02 +0530

    feat:skeleton upgrade to cards

--- frontend/vite-project/src/components/ui/SkeletonCard.jsx
+++ frontend/vite-project/src/components/ui/SkeletonCard.jsx
@@ -1,13 +1,39 @@
 const SkeletonCard = () => {
   return (
-    <div className="card col-12 col-md-6 col-lg-3 my-4">
+    <div className="card my-4 mb-4">
+      
+      {/* Image */}
       <div className="placeholder-glow">
-        <div className="placeholder w-100" style={{ height: "15rem" }}></div>
+        <div
+          className="placeholder w-100"
+          style={{ height: "15rem" }}
+        ></div>
       </div>
+
+      {/* Body */}
       <div className="card-body">
-        <div className="placeholder col-6 mb-2"></div>
-        <div className="placeholder col-7 mb-2"></div>
-        <div className="placeholder col-4"></div>
+        
+        {/* Title */}
+        <div className="placeholder-glow mb-2">
+          <span className="placeholder col-6"></span>
+        </div>
+
+        {/* Description */}
+        <div className="placeholder-glow mb-2">
+          <span className="placeholder col-8"></span>
+          <span className="placeholder col-7"></span>
+        </div>
+
+        {/* Price */}
+        <div className="placeholder-glow mb-3">
+          <span className="placeholder col-4"></span>
+        </div>
+
+        {/* Button */}
+        <div className="placeholder-glow">
+          <span className="placeholder col-6 btn btn-primary disabled"></span>
+        </div>
+
       </div>
     </div>
   );
