ccc052f4ae4c43c5597ddbee79431bff838f9ddb
DIPAKK2310 <dipakkhare217@gmail.com>
2026-04-27 19:28:59 +0530

    feat:modify the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Login from './pages/Login';
 import Logout from './pages/Logout';
 import AdminDashboard from './layout/AdminDashboard';
 import Users from './layout/Users';
-import Products from './layout/Products';
+import AdminProducts from './layout/Products';
 import { Messages } from './layout/Messages';
 import { ThemeProvider } from './context/ThemContext';
 import Cart from './components/Cart';
@@ -17,13 +17,14 @@ import AboutUs from './pages/AboutUs';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsConditions from './pages/TermsConditions';
 import FAQ from './pages/FAQ';
-import ProductsPages from './pages/ProductsPages';
 import AdminDashboardLogin from './components/AdminDashboardLogin';
 import { useState, useEffect } from 'react';
-  import {Toaster} from 'react-hot-toast'; 
+import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './store/AuthContext';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import ProductsPage from './pages/ProductsPage';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
   // Check if the user is authenticated and role is admin
@@ -42,40 +43,41 @@ function App() {
     <Provider store={store}>
       <ThemeProvider>
         <BrowserRouter>
-        <AuthProvider>
+          <AuthProvider>
 
-          <Navbar />
-          <Routes>
-            {/* Public Routes */}
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/aboutUs" element={<AboutUs />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/terms-conditions" element={<TermsConditions />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/ProductsPages" element={<ProductsPages />} />
-            <Route path="/contact" element={<Contact />} />
+            <Navbar />
+            <Routes>
+              {/* Public Routes */}
+              <Route path="/" element={<Home />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/about" element={<AboutUs />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+              <Route path="/terms-conditions" element={<TermsConditions />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/products" element={<ProductsPage />} />
+              <Route path="/products/:id" element={<ProductDetails />} />
+              <Route path="/contact" element={<Contact />} />
 
 
-            {/* Admin Routes (Login Page) */}
-            <Route path="/admin/login" element={<AdminDashboardLogin  onLogin={()=> setIsAuthenticated(true)}/>} />
+              {/* Admin Routes (Login Page) */}
+              <Route path="/admin/login" element={<AdminDashboardLogin onLogin={() => setIsAuthenticated(true)} />} />
 
-            {/* Admin Protected Routes */}
-            <Route path="/admin" element={isAuthenticated ? <AdminDashboard /> : <Navigate to="/admin/login" />}>
-              <Route path="users" element={isAuthenticated ? <Users /> : <Navigate to="/admin/login" />} />
-              <Route path="products" element={isAuthenticated ? <Products /> : <Navigate to="/admin/login" />} />
-              <Route path="messages" element={isAuthenticated ? <Messages /> : <Navigate to="/admin/login" />} />
-            </Route>
+              {/* Admin Protected Routes */}
+              <Route path="/admin" element={isAuthenticated ? <AdminDashboard /> : <Navigate to="/admin/login" />}>
+                <Route path="users" element={isAuthenticated ? <Users /> : <Navigate to="/admin/login" />} />
+                <Route path="Products" element={isAuthenticated ? <Products /> : <Navigate to="/admin/login" />} />
+                <Route path="messages" element={isAuthenticated ? <Messages /> : <Navigate to="/admin/login" />} />
+              </Route>
 
-            {/* Default Redirect */}
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-          <Footer/>
-          <Toaster  position="bottom-right" toastOptions={{duration: 2000}} />
-        </AuthProvider>
+              {/* Default Redirect */}
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+            <Footer />
+            <Toaster position="bottom-right" toastOptions={{ duration: 2000 }} />
+          </AuthProvider>
         </BrowserRouter>
       </ThemeProvider>
     </Provider>
