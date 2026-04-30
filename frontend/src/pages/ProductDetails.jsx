bd9b37765c106a64788b4c65699f1c412f2721f6
DIPAKK2310 <dipakkhare217@gmail.com>
2026-04-30 23:46:51 +0530

    fix:image alt

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -10,104 +10,112 @@ import axios from 'axios';
 
 
 export default function ProductDetail() {
-     
 
-    const {cat,id} =   useParams()
-    const [api,setapi]=useState([])
+
+    const { id } = useParams()
+    const [api, setapi] = useState({})
     const BASE_URL = import.meta.env.VITE_API_URL;
     const [loading, setLoading] = useState(true);
 
 
-    
-    const fetchapi=async()=>{
+
+    const fetchapi = async () => {
         try {
-            
-            const response = await axios(`${BASE_URL}/api/product/products/${cat}/${id}.json`)
-    
+
+            const response = await axios(`${BASE_URL}/api/products/${id}.json`)
+
             const data = response.data
-    
+
             setapi(data)
         } catch (error) {
-                 console.error(error);
-     } finally {
-        setLoading(false);
-    }
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
 
     }
 
-    useEffect(()=>{
+    useEffect(() => {
+        console.log("API DATA:", api);
+
         fetchapi()
-    },[cat, id]);
+    }, [id]);
+
+    if (loading) return <h2>Loading...</h2>;
 
 
-  return (
-    <>
-    <div className="container py-5">
-    
-        <div className="row">
-        {/* image Section  */}
-            <div className="col-12 col-md-6 col-lg-3">
-            <img src={api.imageUrl} alt="selected" className="img-fluid mb-3" />
-            <div className="d-flex">
-                
+    return (
+        <>
+            <div className="container py-5">
+
+                <div className="row">
+                    {/* image Section  */}
+                    <div className="col-12 col-md-6 col-lg-3">
+                        <img
+                            src={api?.imageUrl || ""}
+                            alt="Image"
+                            className="img-fluid mb-3"
+                        />
+                        <div className="d-flex">
+
+                        </div>
+                    </div>
+                    {/* Product Details */}
+
+                    <div className="col-12 col-md-6 col-lg-3">
+                        <h2>{api.title} </h2>
+
+                        <div className="mt-2">
+
+                        </div>
+                        <table className="table mt-2">
+                            <tbody>
+
+                                <tr>
+                                    <td className="fw-bold">Category</td>
+                                    <td>{api.category} </td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+
+                        <div className="mt-2">
+                            <h4>About the product</h4>
+                            <p> {api.description}</p>
+                        </div>
+
+                        <div className="d-flex gap-2 flex-wrap mt-3">
+                            <button
+                                type="button" className="btn btn-danger d-flex align-items-center">
+
+                                <span className="ms-2">ADD TO CART</span>
+
+                            </button>
+
+                            <button
+                                type="button"
+                                className="btn btn-primary d-flex align-items-center"
+                            >
+                                <span className="ms-2">$ {api.price}  BUY NOW</span>
+                            </button>
+                            <button
+                                type="button" className="btn btn-warning d-flex align-items-center">
+
+                                <span className="ms-2">ADD TO WISHLIST</span>
+
+                            </button>
+
+                        </div>
+
+                    </div>
+                    {/* Rewiews Section */}
+                    <div className="col-12 col-md-6 col-lg-3">
+                        <ReviewPage />
+                    </div>
+                </div>
             </div>
-            </div>
-            {/* Product Details */}
-            
-            <div className="col-12 col-md-6 col-lg-3">
-            <h2>{api.title} </h2>
-            
-            <div className="mt-2">
-            
-            </div>
-            <table className="table mt-2">
-                <tbody>
-                
-                <tr>
-                    <td className="fw-bold">Category</td>
-                    <td>{api.category} </td>
-                </tr>
-               
-                </tbody>
-            </table>
+        </>
 
-            <div className="mt-2">
-                <h4>About the product</h4>
-                <p> {api.description}</p>
-            </div>
 
-            <div className="d-flex gap-2 flex-wrap mt-3">
-                <button
-                type="button"  className="btn btn-danger d-flex align-items-center">
-            
-                <span className="ms-2">ADD TO CART</span>
-
-                </button>
-
-                <button
-                type="button"
-                className="btn btn-primary d-flex align-items-center"
-                >
-                <span className="ms-2">$ {api.price}  BUY NOW</span>
-                </button>
-                <button
-                type="button" className="btn btn-warning d-flex align-items-center">
-
-                <span className="ms-2">ADD TO WISHLIST</span>
-
-                </button>
-
-            </div>
-          
-            </div>
-            {/* Rewiews Section */}
-            <div className="col-12 col-md-6 col-lg-3">
-                <ReviewPage/>
-            </div>
-      </div>
-    </div>
-    </>
-    
-    
-  )
+    )
 }
