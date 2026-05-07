c855447b48d53bbb41325fc2f899656984e0832a
DIPAKK2310 <dipakkhare217@gmail.com>
2026-05-07 09:05:26 +0530

    fix:find product by id

--- backend/routes/Product-route.js
+++ backend/routes/Product-route.js
@@ -18,6 +18,24 @@ router.get('/', async (req, res) => {
   }
 });
 
+
+// @route   GET /api/products/:productId
+// @desc    Get single product by ID
+router.get('/:productId', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.productId);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    console.error("Error fetching product:", err);
+    res.status(500).json({ message: 'Error fetching product' });
+  }
+});
+
 // @route   POST /api/products
 // @desc    Add new product
 router.post('/', authenticateAdmin, async (req, res) => {
